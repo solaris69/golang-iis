fc7e9c61eb8bc9a9a2a1d9392f63fd74065e6758
solaris69 <vmanin@outlook.com>
2021-01-22 16:20:31 +0700

    Adapt to my requirement

--- iis/websites/create.go
+++ iis/websites/create.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 )
 
-func (c *WebsitesClient) Create(name string, applicationPool string, physicalPath string, port int32) error {
+func (c *WebsitesClient) Create(name string, applicationPool string, physicalPath string, port int32, domainName string) error {
 	// we normalize the path via PS as otherwise AppSettings/AuthMode fails
 	// when combining `C:\\inetpub\\site\web.config` (also fails on C:/inetpub/site\web.config`)
 	commands := fmt.Sprintf(`
 Import-Module WebAdministration
 $path = [IO.Path]::GetFullPath(%q)
-New-Website -Name %q -ApplicationPool %q -PhysicalPath $path -Port %q
-  `, physicalPath, name, applicationPool, port)
+New-Website -Name %q -ApplicationPool %q -PhysicalPath $path -Port %q -HostHeader %q
+  `, physicalPath, name, applicationPool, port, domainName)
 
 	_, stderr, err := c.Run(commands)
 	if err != nil {
